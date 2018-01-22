e03fe91c946255207a6b655c1a090ce5970f22bc
RETY Mathieu <MRET@syleps.fr>
2018-01-22 18:09:46 +0100

    :lipstick: forgot to import bootstrap

--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import router from './router';
-/* App component */
 import App from './components/App';
-/* App css */
 import './assets/style/global.scss';
 
 /* Twitter Bootstrap JS (this could also be handled in an app.js file) */
-// require('bootstrap');
+require('bootstrap');
 
 Vue.config.productionTip = false;
 

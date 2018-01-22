7702dc36a6eaca313437a525e09292ece0c4fb7d
RETY Mathieu <MRET@syleps.fr>
2018-01-22 18:12:01 +0100

    :sparkles: Add vue-ressource

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import router from './router';
 import App from './components/App';
 import './assets/style/global.scss';
 
 /* Twitter Bootstrap JS (this could also be handled in an app.js file) */
 require('bootstrap');
+
+/* Twitter Bootstrap JS (this could also be handled in an app.js file) */
+Vue.use(VueResource);
 
 Vue.config.productionTip = false;
 

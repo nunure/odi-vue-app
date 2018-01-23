cea0c4e26d71ad0ad84f2b74234efe93f3e4b470
Mathieu RETY <rety.mathieu@gmail.com>
2018-01-23 12:31:37 +0100

    :busts_in_silhouette: filling footer info

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,11 @@
 <template>
   <footer class="ev-appfooter">
     <div class="container">
-      <span class="text-muted">Place sticky footer content here.</span>
+      <span class="text-muted">  
+        Created by: Mathieu RETY - 
+        Contact information: <a href="mailto:rety.mathieu@gmail.com">
+        rety.mathieu@gmail.com</a>.
+      </span>
     </div>
   </footer>
 </template>
@@ -15,6 +19,7 @@
   height: 60px;
   line-height: 60px; /* Vertically center the text there */
   background-color: #f5f5f5;
+  text-align: center;
 }
 .ev-appfooter > .container {
   padding-right: 15px;

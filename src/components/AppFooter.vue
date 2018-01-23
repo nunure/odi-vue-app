ec6c87ccbf5ff67d4025084a8644ac69cb766c78
Mathieu RETY <rety.mathieu@gmail.com>
2018-01-23 12:33:14 +0100

    :rotating_light: removing linter warning

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,8 +1,8 @@
 <template>
   <footer class="ev-appfooter">
     <div class="container">
-      <span class="text-muted">  
-        Created by: Mathieu RETY - 
+      <span class="text-muted">
+        Created by: Mathieu RETY -
         Contact information: <a href="mailto:rety.mathieu@gmail.com">
         rety.mathieu@gmail.com</a>.
       </span>

58c0159a901c3e07e4e292a1b1925c88e4d2bcd8
Mathieu RETY <rety.mathieu@gmail.com>
2018-01-25 17:42:44 +0100

    :rotating_light: correct eslint warning

--- src/components/questionnaire/questionnaire.vue
+++ src/components/questionnaire/questionnaire.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="qcm">
   <h1> Oswestry disability index [ODI]</h1>
-  <div v-for="(subjects, title, index) in questions">
+  <div v-for="(subjects, title, index) in questions" :key="index">
     <h5>{{ title }} </h5>
       <ul>
-        <div class="form-check" v-for="subjects, id in subjects">
+        <div class="form-check" v-for="(subjects, id) in subjects" :key="id">
           <label class="form-check-label">
-            <input class="form-check-input" type="radio" :name="index" :id="'option' + index + '-' + id">
+            <input class="form-check-input" type="radio" :name="index" :id="'option'+index+'-'+id">
               {{ subjects }}
           </label>
         </div>

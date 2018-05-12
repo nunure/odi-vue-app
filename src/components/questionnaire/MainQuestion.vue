125bbe4e27840897ce31ebdd44febf6acfc5c19c
Nunure <rety.mathieu@gmail.com>
2018-05-12 16:23:18 +0200

    :sparkles: slider and calendar working

--- src/components/questionnaire/MainQuestion.vue
+++ src/components/questionnaire/MainQuestion.vue
@@ -1,19 +1,20 @@
 <template>
   <div>
     <question id="qcm">
-      <question-slide class="carousel" v-for="question in datas" :key="question.page"
-      :index="question.page">
+      <question-slide
+        class="carousel"
+        v-for="question in datas"
+        :key="question.page"
+        :index="question.page">
         <form>
           <div v-if="question.page === 0" class="doctor" >
-            <informations></informations>
-            <vue-form-generator  ref="vfg" :schema="question.template" :model="answer.model"
-            :options="formOptions" >
-          </vue-form-generator>
+            <informations/>
+            <vue-form-generator ref="vfg" :schema="question.template" :model="answer.model"
+                                :options="formOptions" />
           </div>
           <div v-else>
             <vue-form-generator ref="vfg" :schema="question.template" :model="answer.model"
-              :options="formOptions" >
-            </vue-form-generator>
+                                :options="formOptions" />
           </div>
         </form>
       </question-slide>
@@ -26,17 +27,23 @@ import Question from '@/components/questionnaire/Question';
 import QuestionSlide from '@/components/questionnaire/QuestionSlide';
 import Informations from '@/components/questionnaire/Informations';
 import QuestionPart1 from '@/components/questionnaire/questions/QuestionPart1';
+import $ from 'jquery';
 import 'ion-rangeslider';
+import Pikaday from 'pikaday';
+import 'pikaday/css/pikaday.css';
 import VueFormGenerator from 'vue-form-generator';
 import 'vue-form-generator/dist/vfg.css'; // optional full css additions
 
+window.$ = $;
+window.Pikaday = Pikaday;
+
 export default {
   components: {
-    "vue-form-generator": VueFormGenerator.component,
+    'vue-form-generator': VueFormGenerator.component,
     Question,
     QuestionSlide,
     Informations,
-    QuestionPart1,
+    QuestionPart1
   },
   data() {
     return {
@@ -45,40 +52,45 @@ export default {
       slides: null,
       formOptions: {
         validateAfterLoad: false,
-        validateAfterChanged: true,
-      },
+        validateAfterChanged: true
+      }
     };
   },
   created() {
     // get all questionnaire
-    this.$http.get('http://localhost:3000/questions').then((response) => {
-      this.datas = response.data;
-      this.slides = Object.keys(this.datas).length;
-    }, response => {
-      // @TODO: handle http error
-      console.error(response);
-  });
+    this.$http.get('http://localhost:3000/questions').then(
+      response => {
+        this.datas = response.data;
+        this.slides = Object.keys(this.datas).length;
+      },
+      response => {
+        // @TODO: handle http error
+        console.error(response);
+      }
+    );
     // get a new empty model for responses
-    this.$http.get('http://localhost:3000/answers/createEmpty').then((response) => {
-      this.answer = response.data;
-    }, response => {
-      // @TODO: handle http error
-      console.error(response);
-  });
-  },
+    this.$http.get('http://localhost:3000/answers/createEmpty').then(
+      response => {
+        this.answer = response.data;
+      },
+      response => {
+        // @TODO: handle http error
+        console.error(response);
+      }
+    );
+  }
 };
-
 </script>
 
 <style scoped>
-  .carousel {
-     left: 0;
-     right: 0;
-     width: 100%;
-     text-align: left;
-     padding-left: 3em;
-     padding-right: 3em;
-  }
+.carousel {
+  left: 0;
+  right: 0;
+  width: 100%;
+  text-align: left;
+  padding-left: 3em;
+  padding-right: 3em;
+}
 
 br {
   display: none;
@@ -101,6 +113,4 @@ h5 {
   align-items: center;
   height: 800px;
 }
-
-
 </style>

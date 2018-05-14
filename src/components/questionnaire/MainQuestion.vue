49af50e02aa7851e783b26d3dedaf96920020eb1
Nunure <rety.mathieu@gmail.com>
2018-05-14 14:02:02 +0200

    :construction: Creation of a html form

--- src/components/questionnaire/MainQuestion.vue
+++ src/components/questionnaire/MainQuestion.vue
@@ -1,5 +1,54 @@
 <template>
   <div>
+    <div
+      class="carousel"
+      v-for="questions in datas"
+      :key="questions.page">
+      <form>
+        <div
+          v-for="question in questions.fields"
+          :key="question.name">
+          <label>{{ question.label }} :</label>
+          <!-- radio button -->
+          <div v-if="question.type === 'radio'">
+            <div
+              v-for="input in question.values"
+              :key="input.value">
+              <input
+                type="radio"
+                :name="question.name"
+                :value="input.value">
+              {{ input.name }}<br>
+            </div>
+          </div>
+          <!-- input type text -->
+          <div v-else-if="question.type === 'input_string'">
+            <input
+              type="text"
+              :name="question.name"
+              :placeholder="question.placeholder">
+          </div>
+          <!-- input type number -->
+          <div v-else-if="question.type === 'input_number'">
+            <input
+              type="number"
+              :name="question.name"
+              :placeholder="question.placeholder">
+          </div>
+          <!-- input type date, max is date of the day for birth day -->
+          <div v-else-if="question.type === 'date'">
+            <input
+              type="date"
+              :name="question.name"
+              min="1900-01-01">
+          </div>
+          <div v-else>
+
+            <p>v else: {{ question.type }}</p>
+          </div>
+        </div>
+      </form>
+    </div>
     <h3>datas</h3>
     <p>{{ datas }}</p>
     <h3>slides</h3>
@@ -12,7 +61,8 @@ export default {
   data() {
     return {
       datas: [],
-      slides: {}
+      slides: {},
+      date: ""
     };
   },
   created() {
@@ -37,6 +87,20 @@ export default {
         console.error(response);
       }
     );
+  },
+  methods: {
+    getDate: function() {
+      var date_tmp = new Date();
+
+      date_tmp.toLocaleDateString("fr-FR");
+      return (
+        date_tmp.getFullYear() +
+        "-" +
+        date_tmp.getMonth() +
+        "-" +
+        date_tmp.getDate()
+      );
+    }
   }
 };
 </script>

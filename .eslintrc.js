3f27d433a6ab375de86bd3c9039968578ca5d74a
Mathieu RETY <rety.mathieu@gmail.com>
2018-10-01 12:05:48 +0200

    :rotating_light: Update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,27 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/html-closing-bracket-newline': 'error',
+    'vue/html-closing-bracket-spacing': 'error',
+    'vue/no-use-v-if-with-v-for': 'error',
+    'vue/no-v-html': 'error',
+    'vue/prop-name-casing': 'error',
+    'vue/script-indent': 'error'
   },
+
   parserOptions: {
-    parser: "babel-eslint"
-  }
+    parser: 'babel-eslint'
+  },
+
+  'extends': [
+    'plugin:vue/recommended',
+    '@vue/prettier'
+  ]
 };

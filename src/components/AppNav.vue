d62f1b7692b33738cf4b3153e6c262df1f25c9e6
Mathieu RETY <rety.mathieu@gmail.com>
2019-01-25 15:22:40 +0100

    :hammer: Connection page

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,35 +1,41 @@
 <template>
   <div class="app-nav">
-    <el-menu :default-active="activeIndex" :router="true" mode="horizontal">
-      <el-menu-item index="/">
-        <router-link tag="a" to="/">
-          <a class="navbar-brand nav-link">
-            <img src="../assets/images/logo.png" class="ev-appnav__logo" />
-          </a>
-        </router-link>
-      </el-menu-item>
-      <el-menu-item index="/">Accueil</el-menu-item>
-      <el-menu-item index="/questions">Questions</el-menu-item>
+    <el-row :gutter="20" type="flex" class="main-body" style="margin: 0px">
+      <el-col :span="5" class="hidden-sm-and-down" />
+      <el-col :span="18" style="width: 100%">
+        <el-menu :default-active="activeIndex" :router="true" mode="horizontal">
+          <el-menu-item index="/">
+            <router-link tag="a" to="/">
+              <a class="navbar-brand nav-link">
+                <img src="../assets/images/logo.png" class="ev-appnav__logo" />
+              </a>
+            </router-link>
+          </el-menu-item>
+          <el-menu-item index="/">Accueil</el-menu-item>
+          <el-menu-item v-if="authenticated" index="/questions">
+            Questions
+          </el-menu-item>
 
-      <el-menu-item
-        v-if="!authenticated"
-        id="auth-menu"
-        index=""
-        @click="auth.login()"
-      >
-        Log In
-      </el-menu-item>
-      <el-menu-item
-        v-if="authenticated"
-        id="auth-menu"
-        index=""
-        @click="auth.logout()"
-      >
-        Log Out
-      </el-menu-item>
-      <!-- <el-menu-item index="/questionnaire">Questionnaire</el-menu-item>
-      <el-menu-item index="3">Orders</el-menu-item> -->
-    </el-menu>
+          <el-menu-item
+            v-if="!authenticated"
+            id="auth-menu"
+            index=""
+            @click="auth.login()"
+          >
+            Log In
+          </el-menu-item>
+          <el-menu-item
+            v-if="authenticated"
+            id="auth-menu"
+            index=""
+            @click="auth.logout()"
+          >
+            Log Out
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+      <el-col :span="5" class="hidden-sm-and-down" />
+    </el-row>
   </div>
 </template>
 
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>Bienvenue sur l'applcaition d'auto évaluation de votre consulation</h1>
+    <span id="subtitle">Commencez par vous connecter:</span> <br /><br />
+
+    <el-button v-if="!authenticated" @click="auth.login()"> Log In </el-button>
+    <el-button v-if="authenticated" @click="auth.logout()"> Log Out </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    auth: { type: Object, required: true },
+    authenticated: { type: Boolean, default: false, required: true }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
